e9949559359086e1089e92096c9e28e652bff6e5
Riya Dey <1805013@ugrad.cse.buet.ac.bd>
2024-01-30 03:36:33 +0600

    Bank created and option 1 has been done successfully

---
+++ simple_bank_application/src/main/java/org/example/Bank.java
@@ -0,0 +1,42 @@
+package org.example;
+
+import java.util.HashMap;
+import java.util.List;
+
+public final class Bank {
+    private String name;
+    private static Bank INSTANCE;
+    private HashMap<String, List<Account>> userToAccount;
+    private HashMap<String,Account> accountNumberToAccount;
+    private int accountCnt;
+
+    private Bank(){
+        accountCnt = 0;
+    }
+    public static Bank getInstance(){
+        if(INSTANCE == null)    INSTANCE = new Bank();
+        return INSTANCE;
+    }
+
+    public void createAccount(String name, double balance,String type){
+        // The new account number will be existing maximum account number + 1
+        accountCnt++;
+        String newAccountUuid = Integer.toString(accountCnt);
+
+        AccountType accountType = AccountType.valueOf(type);
+        Account createdAccount ;
+        if(accountType == AccountType.SAVINGS){
+            createdAccount = new SavingsAccount(name,newAccountUuid,balance);
+        }
+        else if(accountType == AccountType.SALARY){
+            createdAccount = new SalaryAccount(name,newAccountUuid,balance);
+        }
+        else{
+            createdAccount = new CurrentAccount(name,newAccountUuid,balance);
+        }
+        userToAccount.get(name).add(createdAccount);
+        accountNumberToAccount.put(newAccountUuid,createdAccount);
+
+    }
+
+}
