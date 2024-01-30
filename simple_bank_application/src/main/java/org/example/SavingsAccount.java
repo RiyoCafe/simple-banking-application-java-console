e1872486cf57b3295aeada5ed965f0f599aa88a0
Riya Dey <1805013@ugrad.cse.buet.ac.bd>
2024-01-31 03:04:59 +0600

    Done and tested

--- simple_bank_application/src/main/java/org/example/SavingsAccount.java
+++ simple_bank_application/src/main/java/org/example/SavingsAccount.java
@@ -1,13 +1,11 @@
 package org.example;
 
 public class SavingsAccount extends Account {
-    final double SAVINGS_OPENING_LIMIT = 100000;
-    final double SAVINGS_MAINTENANCE_LIMIT = 50000;
 
     public SavingsAccount(String name, String number, double balance) {
         super(name, number, balance);
-        super.setAccountOpeningLimit(SAVINGS_OPENING_LIMIT);
-        super.setAccountMaintenanceLimit(SAVINGS_MAINTENANCE_LIMIT);
+        super.setAccountOpeningLimit(CONSTANTS.SAVINGS_OPENING_LIMIT);
+        super.setAccountMaintenanceLimit(CONSTANTS.SAVINGS_MAINTENANCE_LIMIT);
         super.setAccountType(AccountType.SAVINGS);
     }
 }
